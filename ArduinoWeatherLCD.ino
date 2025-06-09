d33d790738414b541083b27ea1b43bf4a7c0313b
Markoulidakis Panagiotis <panosm763@gmail.com>
2025-06-09 18:38:14 +0300

    uv index
    added uv index, dew point

--- ArduinoWeatherLCD.ino
+++ ArduinoWeatherLCD.ino
@@ -11,7 +11,7 @@ const int updateInterval = 4000; // 5 seconds
 
 // BMP180 setup
 Adafruit_BMP085 bmp;
-
+float localSeaLevelPressure;
 // MQ-135 setup
 #define MQ135_PIN 34  // Analog pin for MQ-135
 #define RL 10000  // MQ-135 10k Ohm load resistor
@@ -21,8 +21,12 @@ Adafruit_BMP085 bmp;
 #define DHT_TYPE  DHT11 // DHT11 sensor
 DHT dht(DHT_PIN, DHT_TYPE);
 
+//UV sensor
+#define UV_PIN 36
+
 void setup() {
   Serial.begin(115200);
+
 
   // Initialize LCD
   lcd.init();
@@ -36,7 +40,16 @@ void setup() {
     Serial.println("Could not find BMP180 sensor.");
     while (1) {}
   }
-
+// Use data from a well know local area calibrated weather station
+  float referencePressure = 1011.31; // hPa
+  float referenceAltitude = 40.0;   // meters
+  float referenceTemp = 31.4;       // Celsius
+  
+  localSeaLevelPressure = calibrateSeaLevelPressure(referencePressure, referenceAltitude, referenceTemp);
+  Serial.print("Calibrated Sea-Level Pressure: ");
+  Serial.print(localSeaLevelPressure, 1);
+  Serial.println(" hPa");
+  
   lcd.setCursor(0, 0);
   lcd.print("BMP180 OK!");
 
@@ -46,7 +59,7 @@ void loop() {
   // Read temperature, pressure, and altitude
   float temp = bmp.readTemperature();               // Celsius
   float pressure = bmp.readPressure() / 100.0;      // hPa
-  float altitude = bmp.readAltitude();       // Altitude in meters, assuming sea-level pressure of 1013.25 hPa
+  float altitude = bmp.readAltitude(localSeaLevelPressure);       // Altitude in meters, assuming sea-level pressure of 1013.25 hPa
 
   float humidity = dht.readHumidity();
   float dhtTemp = dht.readTemperature();
@@ -54,6 +67,14 @@ void loop() {
   int analogValue = analogRead(MQ135_PIN);
   float voltage = analogValue * (3.3 / 4095.0);
   float Rs = (3.3 - voltage) / voltage * RL;
+
+  int uvRaw = analogRead(UV_PIN);
+  // Convert raw ADC to voltage (3.3V ref, 12-bit ADC)
+  float uvVoltage = uvRaw * (3.3 / 4095.0);
+
+  // Approximate UV Index
+  float uvIndex = (uvVoltage - 0.99) / 0.06;
+  if (uvIndex < 0) uvIndex = 0; // No negative index
 
   // Display on LCD
   if (millis() - lastUpdate >= updateInterval) {
@@ -105,15 +126,57 @@ void loop() {
         }
         break;
 
-      // Add more cases if you want more screens
+        case 3:
+          lcd.setCursor(0, 0);
+          lcd.print("UV Index:");
+          lcd.print(uvIndex, 1);
+
+          lcd.setCursor(0, 1);
+          lcd.print("Voltage:");
+          lcd.print(uvVoltage, 2);
+          lcd.print(" V");
+          break;
+          
+        case 4:
+          lcd.setCursor(0, 0);
+          lcd.print("Dew Point: ");
+          if (!isnan(dhtTemp) && !isnan(humidity)) {
+            lcd.print("Error");
+          } else {
+            float dewPoint = calculateDewPoint(dhtTemp, humidity);
+            lcd.print((char)223);
+            lcd.print("C");
+          }
+        break;
+
     }
 
     // Cycle to the next screen
     screenIndex++;
-    if (screenIndex > 2) screenIndex = 0;
+    if (screenIndex > 4) screenIndex = 0;
 
     lastUpdate = millis();
   }
 
 
+}
+
+float calculateDewPoint(float tempC, float humidity) {
+  const float a = 17.62;
+  const float b = 243.12;
+
+  float gamma = (a * tempC) / (b + tempC) + log(humidity / 100.0);
+  float dewPoint = (b * gamma) / (a - gamma);
+
+  return dewPoint;
+}
+
+float calibrateSeaLevelPressure(float referencePressure, float referenceAltitude, float referenceTemp) {
+  const float lapseRate = 0.0065;
+  float tempK = referenceTemp + 273.15;
+
+  float seaLevelPressure = referencePressure *
+                            pow(1 - (lapseRate * referenceAltitude) / (tempK + lapseRate * referenceAltitude), -5.257);
+
+  return seaLevelPressure;
 }
